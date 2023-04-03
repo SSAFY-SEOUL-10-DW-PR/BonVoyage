a191ab30a93c0a6a9c3a3215ecf094f3c36590f2
prkim99 <prkim99@gmail.com>
2023-04-03 14:18:08 +0900

    관광지 관련 모델구조 추가

---
+++ enjoytrip_backend_seoul_10_DW_PR/src/com/enjoytrip/attraction/model/service/AttractionServiceImpl.java
@@ -0,0 +1,16 @@
+package com.enjoytrip.attraction.model.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ssafy.board.model.BoardDto;
+import com.ssafy.board.model.dao.BoardDao;
+import com.ssafy.board.model.dao.BoardDaoImpl;
+import com.ssafy.util.PageNavigation;
+import com.ssafy.util.SizeConstant;
+
+public class AttractionServiceImpl implements AttractionService {
+	
+
+}
